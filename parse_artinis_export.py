57d3466def56bfc1b5495f5997b7747535699389
William Liu <will123liu@gmail.com>
2022-08-12 13:29:11 -0700

    Move code outside of with context manager

--- parse_artinis_export.py
+++ parse_artinis_export.py
@@ -2,58 +2,61 @@ import pandas as pd
 
 def parse_artinis_export(file_path: str) -> pd.DataFrame:
     """Read the exported file from Artinis fNIRS software and parse into a pandas DataFrame."""
+    lines = None
     with open(file_path, 'r') as f:
-
         # Split the .txt file into lines
         lines = f.read().split('\n')
 
-        # Split each line into columns
-        rows = [[i for i in j.split('\t')] for j in lines]
-
-        # Get column labels to use for DataFrame, also get sample rate
-        start = None
-        end = None
-        sample_rate = None
-        for idx, row in enumerate(rows):
-            if "Datafile sample rate:" in row:
-                sample_rate = int(float(row[1]))
-            elif "(Sample number)" in row:
-                start = idx
-            elif "(Event)" in row:
-                end = idx
-                break
-        if start is not None and end is not None and sample_rate is not None:
-            col_labels = rows[start:(end + 1)]
-            col_labels = [i[1] for i in col_labels]
-        else:
-            raise ValueError(f"""Could not find start, end, or sample rate in the .txt file. 
-            Start: {start}, end: {end}, sample_rate: {sample_rate}""")
-
-        # Remove extra characters from column labels
-        for idx, label in enumerate(col_labels):
-            if "O2Hb" in label or "HHb" in label:
-                new_label = label.split('(')[0].rstrip()
-                col_labels[idx] = new_label
-            elif "(Sample number)" in label or "(Event)" in label:
-                new_label = label.split('(')[1].split(')')[0]
-                col_labels[idx] = new_label
-            else:
-                raise KeyError(f"Unexpected value found in column labels: {label}")
-
-        # Create DataFrame
-        data = rows[(end + 4):-1]  # Last line is empty, ignore it
-        for idx, row in enumerate(data):
-            if len(row) == len(col_labels) + 1:
-                data[idx].pop()
-            elif len(row) == len(col_labels):
-                pass
-            else:
-                raise ValueError(f"""Unexpected number of items in row {idx}. 
-                Expected {len(col_labels)}, found {len(row)}.""")
+    if lines is None:
+        raise AttributeError(f"Could not open {file_path}.")
         
-        df = pd.DataFrame(data=data, columns=col_labels)
+    # Split each line into columns
+    rows = [[i for i in j.split('\t')] for j in lines]
 
-        # Drop initial 1 second of recording
-        df.drop(df.index[range(sample_rate)], inplace=True)
+    # Get column labels to use for DataFrame, also get sample rate
+    start = None
+    end = None
+    sample_rate = None
+    for idx, row in enumerate(rows):
+        if "Datafile sample rate:" in row:
+            sample_rate = int(float(row[1]))
+        elif "(Sample number)" in row:
+            start = idx
+        elif "(Event)" in row:
+            end = idx
+            break
+    if start is not None and end is not None and sample_rate is not None:
+        col_labels = rows[start:(end + 1)]
+        col_labels = [i[1] for i in col_labels]
+    else:
+        raise ValueError(f"""Could not find start, end, or sample rate in the .txt file. 
+        Start: {start}, end: {end}, sample_rate: {sample_rate}""")
+
+    # Remove extra characters from column labels
+    for idx, label in enumerate(col_labels):
+        if "O2Hb" in label or "HHb" in label:
+            new_label = label.split('(')[0].rstrip()
+            col_labels[idx] = new_label
+        elif "(Sample number)" in label or "(Event)" in label:
+            new_label = label.split('(')[1].split(')')[0]
+            col_labels[idx] = new_label
+        else:
+            raise KeyError(f"Unexpected value found in column labels: {label}")
+
+    # Create DataFrame
+    data = rows[(end + 4):-1]  # Last line is empty, ignore it
+    for idx, row in enumerate(data):
+        if len(row) == len(col_labels) + 1:
+            data[idx].pop()
+        elif len(row) == len(col_labels):
+            pass
+        else:
+            raise ValueError(f"""Unexpected number of items in row {idx}. 
+            Expected {len(col_labels)}, found {len(row)}.""")
+    
+    df = pd.DataFrame(data=data, columns=col_labels)
+
+    # Drop initial 1 second of recording
+    df.drop(df.index[range(sample_rate)], inplace=True)
 
     return df
