3c9585680c98178dd8ad9e88270791e5a916698b
William Liu <will123liu@gmail.com>
2022-08-12 12:54:54 -0700

    Add process.py file

---
+++ process.py
@@ -0,0 +1,10 @@
+from parse_artinis_export import parse_artinis_export
+import pandas as pd
+import os
+import re
+import numpy as np
+from scipy.signal import butter, sosfiltfilt
+import math
+
+def process():
+    pass
