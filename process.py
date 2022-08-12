a12c2495d07defa04a286ed37922fe5e03d6e2f4
William Liu <will123liu@gmail.com>
2022-08-12 13:14:28 -0700

    Add method to transform DataFrame to array and get labeled events

--- process.py
+++ process.py
@@ -5,6 +5,20 @@ import re
 import numpy as np
 from scipy.signal import butter, sosfiltfilt
 import math
+from typing import Union
 
 def process():
     pass
+
+def transform_data(df: pd.DataFrame) -> Union[np.array, pd.DataFrame]:
+    """
+    Create a numpy array of raw data from the DataFrame. Return numpy array without "Sample number" or "Event columns.
+    Return a separate DataFrame with only the rows containing Event markers.
+    """
+    # DataFrame to numpy array
+    df_asarray = df.iloc[:, 1:-1].to_numpy(dtype=np.float64)
+
+    # DataFrame of events
+    events = df[df['Event'] != '']
+
+    return df_asarray, events
