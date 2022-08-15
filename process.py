9ebb8b684d09c8400d486198a7b4d8388f371b53
William Liu <will123liu@gmail.com>
2022-08-15 12:20:19 -0700

    Add method definition for tddr

--- process.py
+++ process.py
@@ -10,7 +10,7 @@ from typing import Union
 def process():
     pass
 
-def transform_data(df: pd.DataFrame) -> Union[np.array, pd.DataFrame]:
+def _transform_data(df: pd.DataFrame) -> Union[np.array, pd.DataFrame]:
     """
     Create a numpy array of raw data from the DataFrame. Return numpy array without "Sample number" or "Event columns.
     Return a separate DataFrame with only the rows containing Event markers.
@@ -22,3 +22,14 @@ def transform_data(df: pd.DataFrame) -> Union[np.array, pd.DataFrame]:
     events = df[df['Event'] != '']
 
     return df_asarray, events
+
+def _tddr(data: np.array) -> np.array:
+    """
+    Apply Temporal Derivative Distribution Repair algorithm.
+
+    Fishburn F.A., Ludlum R.S., Vaidya C.J., & Medvedev A.V. (2019).
+    Temporal Derivative Distribution Repair (TDDR): A motion correction
+    method for fNIRS. NeuroImage, 184, 171-179.
+    https://doi.org/10.1016/j.neuroimage.2018.09.025
+    """
+    pass
