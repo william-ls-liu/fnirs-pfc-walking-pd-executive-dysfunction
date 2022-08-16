1919df512e709b1e6b481e1f535e5f119de6705b
William Liu <will123liu@gmail.com>
2022-08-16 10:47:47 -0700

    Rename channel to ch to maintain consistency with other methods

--- process.py
+++ process.py
@@ -22,10 +22,10 @@ def _transform_data(df: pd.DataFrame, short_chs: list) -> Union[np.array, pd.Dat
     # Get DataFrame with only the long channels
     all_chs = list(df.columns)
     long_chs = list()
-    for channel in all_chs:
-        if not ('Sample number' in channel or 'Event' in channel):
-            if not re.match(short_regex, channel):
-                long_chs.append(channel)
+    for ch in all_chs:
+        if not ('Sample number' in ch or 'Event' in ch):
+            if not re.match(short_regex, ch):
+                long_chs.append(ch)
     long_regex = '|'.join(long_chs)
     long_data = df.filter(regex=long_regex)
 
