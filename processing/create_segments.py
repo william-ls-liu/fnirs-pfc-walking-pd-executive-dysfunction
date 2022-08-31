53e77a7f3ee5b101a819696a2ad685c2e588da36
William Liu <will123liu@gmail.com>
2022-08-31 11:28:59 -0700

    Add method to separate fnirs data into segments.
    The trials are tagged with events that are generated by user input. For
    this particular study the data should be tagged with three events. There
    is a period of ~20s of quiet stance, ~80s walking, and then more quiet
    stance at the end. The period of walking is also split in half to create
    an 'early' and 'late' phase.

--- processing/create_segments.py
+++ processing/create_segments.py
@@ -1,3 +1,40 @@
 # Author: William Liu <liwi@ohsu.edu>
 
 import pandas as pd
+
+
+def create_segments(df: pd.DataFrame) -> dict:
+    """
+    Split trial into segments based on 'Event' markers.
+
+    :param df: dataframe of processed fnirs
+    :return: dict with keys as individual segments and values
+             as dataframes with fnirs data for given segment
+    """
+    if type(df) != pd.DataFrame:
+        raise TypeError(f"Must provide dataframe, not {type(df)}.")
+    
+    events = df[df['Event'].notnull()]
+    if len(events) != 3:
+        raise IndexError(f"Expected 3 event markers, found {len(events)}.")
+
+    # Define a dictionary to store dataframe for each segment
+    segments = dict()
+    # Use event markers to define 2 segments. One is quiet stance, other is walking
+    for idx, seg in enumerate(['Quiet Stance', 'Walking']):
+        start = events.index[idx]
+        end = events.index[idx + 1]
+        segments[seg] = df.iloc[start:end]
+    # Create early and late phase segments during walking
+    start = events.index[1]
+    end = events.index[2]
+    gap = end - start
+    mid = gap // 2
+    mid += start
+    early = df.iloc[start:mid]
+    late = df.iloc[mid:end]
+    # Add early and late to segments
+    segments['early'] = early
+    segments['late'] = late
+
+    return segments
