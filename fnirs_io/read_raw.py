81f8136954f017f59acf90a40ac8b3fcec090064
William Liu <will123liu@gmail.com>
2022-09-02 11:39:49 -0700

    'Call read_mat method if provided file is .mat'

--- fnirs_io/read_raw.py
+++ fnirs_io/read_raw.py
@@ -2,15 +2,15 @@
 
 import pandas as pd
 import os
-from . import _read_txt
 from ._read_txt import read_txt
+from ._read_mat import read_mat
 
 def read_raw(file_path: str):
     filename, file_extension = os.path.splitext(file_path)
     if file_extension == '.txt':
         raw_fnirs = read_txt(file_path)
     elif file_extension == '.mat':
-        pass
+        raw_fnirs = read_mat(file_path)
     else:
         raise TypeError(f"File provided in {file_extension} format. Expected .txt or .mat.")
     
