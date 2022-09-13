0ef58648a47bc3e8ce1756c446e8cf3a59a3da75
William Liu <will123liu@gmail.com>
2022-09-13 10:52:33 -0700

    Use pandas series instead of numpy array.

--- processing/baseline.py
+++ processing/baseline.py
@@ -13,15 +13,14 @@ def baseline_subtraction(data: pd.DataFrame, events: pd.DataFrame, frames_to_dro
     """
     if len(events) != 3:
         raise ValueError(f"The number of events found was {len(events)}, expected 3.")
+
     corrected_df = data.copy()
-    # Subtract number of frames that were dropped initially to align with numpy indexing
-    start = events['Sample number'].iloc[0] - frames_to_drop
-    end = events['Sample number'].iloc[1] - frames_to_drop
+    start = events['Sample number'].iloc[0]
+    end = events['Sample number'].iloc[1]
     for ch in list(data.columns):
-        ch_asarray = np.array(data[ch], dtype='float64')
-        quiet_stance = ch_asarray[start:end]
+        quiet_stance = data.loc[start:(end - 1), ch]
         quiet_stance_mean = math.fsum(quiet_stance) / len(quiet_stance)
-        ch_asarray -= quiet_stance_mean
-        corrected_df[ch] = ch_asarray
+        baseline_removed = data.loc[:, ch] - quiet_stance_mean
+        corrected_df[ch] = baseline_removed
     
     return corrected_df
