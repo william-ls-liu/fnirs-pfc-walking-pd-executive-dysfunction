14fbe4f3bf875960bc18c278a4c11bdddcc6b420
William Liu <will123liu@gmail.com>
2022-09-19 11:10:37 -0700

    fix: Use copy of dataframe instead of the original

--- processing/baseline.py
+++ processing/baseline.py
@@ -20,11 +20,11 @@ def baseline_subtraction(data: pd.DataFrame, events: pd.DataFrame):
     corrected_df = data.copy()
     start = events['Sample number'].iloc[0]
     end = events['Sample number'].iloc[1]
-    for ch in list(data.columns):
+    for ch in list(corrected_df.columns):
         # Stop slice is included with df.loc, so subtract 1
-        quiet_stance = data.loc[start:(end - 1), ch]
+        quiet_stance = corrected_df.loc[start:(end - 1), ch]
         quiet_stance_mean = math.fsum(quiet_stance) / len(quiet_stance)
-        baseline_removed = data.loc[:, ch] - quiet_stance_mean
+        baseline_removed = corrected_df.loc[:, ch] - quiet_stance_mean
         corrected_df[ch] = baseline_removed
 
     return corrected_df
