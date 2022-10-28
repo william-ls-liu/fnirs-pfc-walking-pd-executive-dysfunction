0cf12ee8124f8a3e2ae0ddf901b13cce1cce270e
William Liu <will123liu@gmail.com>
2022-10-28 10:13:31 -0700

    fix: Add check to ensure consistent dtype.

--- fnirs_io/read_raw.py
+++ fnirs_io/read_raw.py
@@ -1,9 +1,9 @@
 # Author: William Liu <liwi@ohsu.edu>
 
-import pandas as pd
 import os
 from ._read_txt import read_txt
 from ._read_mat import read_mat
+
 
 def read_raw(file_path: str):
     filename, file_extension = os.path.splitext(file_path)
@@ -12,6 +12,12 @@ def read_raw(file_path: str):
     elif file_extension == '.mat':
         raw_fnirs = read_mat(file_path)
     else:
-        raise TypeError(f"File provided in {file_extension} format. Expected .txt or .mat.")
-    
+        raise TypeError(f"""{filename} provided in {file_extension} format.
+        Expected .txt or .mat.""")
+
+    # Check to ensure numerical cols are of dtype float64
+    if not all(raw_fnirs['data'].iloc[:, 1:-1].dtypes == 'float64'):
+        raise TypeError("Unexpected type found in raw dataframe for "
+                        f"{filename}.")
+
     return raw_fnirs
